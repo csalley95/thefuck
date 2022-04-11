19e775c36e0447263be34a243b5e7221c4d225db
Ryan Valler <rwv1293@gmail.com>
2022-04-11 18:46:37 -0400

    made modifications to  lowercase rule to attempt to fix issue 802

--- thefuck/rules/lowercase.py
+++ thefuck/rules/lowercase.py
@@ -1,14 +1,16 @@
 
 
 def match(command):
-    return sum([ch.isupper() for ch in command.script]) > sum([ch.islower() for ch in command.script])
+    # return sum([ch.isupper() for ch in command.script]) > sum([ch.islower() for ch in command.script])
+    if command.script.isupper():
+        return True
 
 
 def get_new_command(command):
     return command.script.lower()
 
 
-priority = 1200
+priority = 1500
 
 requires_output = False
 
