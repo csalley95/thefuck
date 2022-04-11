2a34d2f1ff6b7d84cc28968d95263199f60d8559
Ryan Valler <rwv1293@gmail.com>
2022-04-11 17:39:41 -0400

    made modifications to  lowercase rule to increase its priority.

--- thefuck/rules/lowercase.py
+++ thefuck/rules/lowercase.py
@@ -5,9 +5,19 @@ requires_output = False
 enabled_by_default = True
 
 
+#
+# def match(command):
+#     return sum([ch.isupper() for ch in command.script]) > sum([ch.islower() for ch in command.script])
+#
+#
+# def get_new_command(command):
+#     return command.script.lower()
+
+
 def match(command):
-    return sum([ch.isupper() for ch in command.script]) > sum([ch.islower() for ch in command.script])
+    if command.script_parts[0] == 'CD':
+        return True
 
 
 def get_new_command(command):
-    return command.script.lower()
+    return 'cd' + ''.join(command.script[2:])
