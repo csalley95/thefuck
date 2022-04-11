73b6865f0b8e572b07b3c1e668d08857a865d161
Ryan Valler <rwv1293@gmail.com>
2022-04-11 17:01:15 -0400

    added a lowercase rule to apply fix to individual issue RV

---
+++ thefuck/rules/lowercase.py
@@ -0,0 +1,13 @@
+priority = 1200
+
+requires_output = False
+
+enabled_by_default = True
+
+
+def match(command):
+    return sum([ch.isupper() for ch in command.script]) > sum([ch.islower() for ch in command.script])
+
+
+def get_new_command(command):
+    return command.script.lower()
